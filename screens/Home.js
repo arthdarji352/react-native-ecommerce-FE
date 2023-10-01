0237b3a4d9f4ea1388d40bae5fbdcd5b4298b139
arthdarji352 <arthdarji20@gmail.com>
2023-10-01 11:44:04 +0530

    routing,layout,categories done

---
+++ screens/Home.js
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import Layout from "../components/Layout";
+import Categories from "../components/category/Categories";
+
+const Home = () => {
+  return (
+    <Layout>
+      <Categories />
+      <View>
+        <Text>Home Page</Text>
+      </View>
+    </Layout>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
