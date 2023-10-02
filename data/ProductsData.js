2a66e163483f90c927cfc4588b5c1b6081433de6
arthdarji352 <arthdarji20@gmail.com>
2023-10-02 19:09:37 +0530

    Product Details page Done with add cart button

--- data/ProductsData.js
+++ data/ProductsData.js
@@ -4,7 +4,8 @@ export const ProductsData = [
     name: "Apple iPhone 15 Plus (256 GB) - Pink",
     description:
       "DYNAMIC ISLAND COMES TO IPHONE 15, INNOVATIVE DESIGN 48MP MAIN CAMERA WITH 2X TELEPHOTO",
-    price: "900",
+    price: 900,
+    quantity: 30,
     category: "mobile",
     imageUrl: "https://m.media-amazon.com/images/I/71bErtQPC3L._SL1500_.jpg",
   },
